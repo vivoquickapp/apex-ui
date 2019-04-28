369dae78d54ef82d9aeb64148c2ad89450ddbc23
zhouxiaodong <xiaodong.zhou@vivo.com>
2019-04-28 18:03:46 +0800

    init project

---
+++ components/common/scripts/util.js
@@ -0,0 +1,17 @@
+export function addNum (num1, num2) {
+    let sq1, sq2, m;
+    try {
+        sq1 = num1.toString().split('.')[1].length;
+    }
+    catch (e) {
+        sq1 = 0;
+    }
+    try {
+        sq2 = num2.toString().split('.')[1].length;
+    }
+    catch (e) {
+        sq2 = 0;
+    }
+    m = Math.pow(10, Math.max(sq1, sq2));
+    return (Math.round(num1 * m) + Math.round(num2 * m)) / m;
+}
